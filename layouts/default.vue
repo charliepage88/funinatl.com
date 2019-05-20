69ff51517cb39698618380dc73e13ecfbe52d52f
Charlie Page <charliepage88@gmail.com>
2019-05-20 13:36:54 -0400

    m

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+      <div class="mb-2 sm:mb-0 flex flex-row
+      ">
+        <div class="bg-no-repeat bg-center w-1/2" style="background-image: url('/images/funinatl-bg.png');">
+          <a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">FunInATL</a>
+        </div>
+      </div>
+
+      <div class="sm:mb-0 self-center">
+        <!-- <div class="h-10" style="display: table-cell, vertical-align: middle;"> -->
+        <a href="#" class="text-md no-underline text-black hover:text-blue-dark ml-2 px-1">Link1</a>
+        <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1">Link2</a>
+        <!-- <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">About Us</a> -->
+        <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1">Link3</a>
+        <!-- </div> -->
+
+      </div>
+    </nav>
+
     <div class="container">
       <nuxt />
     </div>
