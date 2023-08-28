c345dcb4ecda81a24f816af4f5e22ff48e883a7b
Charlie Page <charliepage88@gmail.com>
2023-08-27 22:53:36 -0400

    progress from weekend

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+});
+</script>
+
+<script>
+import Layout from '@/Layouts/AppLayout.vue'
+
+export default {
+    // Using a render function...
+    layout: (h, page) => h(Layout, [page]),
+
+    // Using shorthand syntax...
+    layout: Layout,
+}
+</script>
+
+<template>
+    <Head title="Home" />
+
+    hello this is index
+</template>
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -3,10 +3,16 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+
+    <title>FunInATL Prototype</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
   </head>
   <body>
     @inertia
+    <div id="app">
+      {{ $slot }}
+    </div>
   </body>
 </html>
